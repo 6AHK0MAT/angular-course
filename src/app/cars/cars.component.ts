f8b8938d69415f0cf4a9474a8c3699a11ac87861
6AHK0MAT <makhmutov.va@mail.ru>
2018-02-10 22:37:19 +0700

    Блок 2 ngif else

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -7,18 +7,13 @@ import { Component } from '@angular/core';
 })
 export class CarsComponent {
 
-    addCarStatus = '';
-    inputText = 'Default text';
-
+    carName = '';
+    addCarStatus =false;
     constructor() {
     }
 
     addCar() {
-        this.addCarStatus = 'Машина добавлена!';
-    }
-
-    onKeyUp(event) {
-        this.inputText = event.target.value;
+        this.addCarStatus = true;
     }
 
 }
