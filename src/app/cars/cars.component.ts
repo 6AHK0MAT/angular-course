3aa9fab6793804ff44278594f5fd2157feea0e8e
6AHK0MAT <makhmutov.va@mail.ru>
2018-02-10 23:15:13 +0700

    Блок 2 ngStyle ngClass

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -9,8 +9,7 @@ export class CarsComponent {
 
     carName = '';
     addCarStatus =false;
-    cars =['Ford', 'Audi', 'BMW'];
-    items = [{id: 3, name: 'item 1'}, {id: 6, name: 'item 2'}, {id: 9, name: 'item 3'}];
+    cars =['Ford', 'Audi', 'BMW', 'Mazda', 'Lada', 'Bently'];
 
     constructor() {
     }
@@ -19,6 +18,10 @@ export class CarsComponent {
         this.addCarStatus = true;
         this.cars.push(this.carName);
         this.carName = '';
+    }
+
+    setBigCarText(car: string){
+        return car.length > 4 ? true : false;
     }
 
 }
