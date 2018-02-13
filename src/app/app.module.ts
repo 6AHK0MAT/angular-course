1142c67ae7885e85c659922f3d27ec291d499287
6AHK0MAT <makhmutov.va@mail.ru>
2018-02-13 17:42:47 +0700

    Блок 4. Теория. Директивы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
-import { CarComponent } from './car/car.component';
-import { AddCarComponent } from './add-car/add-car.component';
+import {AppComponent} from './app.component';
+import {BackgroundDirective} from './directives/background.directive';
+import { MyDirectiveDirective } from './directives/my-directive.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CarsComponent,
-    CarComponent,
-    AddCarComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        BackgroundDirective,
+        MyDirectiveDirective
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/directives/background.directive.ts
@@ -0,0 +1,14 @@
+import {Directive, ElementRef, OnInit} from '@angular/core';
+
+@Directive({
+    selector: '[appBackrounnd]'
+})
+export class BackgroundDirective implements OnInit {
+    constructor(private element: ElementRef) {
+    }
+
+    ngOnInit() {
+        // console.log(this.element);
+        this.element.nativeElement.style.backgroundColor = 'red';
+    }
+}
