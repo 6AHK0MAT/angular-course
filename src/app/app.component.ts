4846e38da1a5271a91f94ab428e74f8cf50d3256
6AHK0MAT <makhmutov.va@mail.ru>
2018-02-14 17:37:39 +0700

    Блок 4. Теория. Директивы Директива ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  headerText = 'Создание своей директивы.';
+    headerText = 'Создание своей директивы.';
+    items = [1, 2, 3, 4, 5];
+    current = 1;
+
+    onClick(number: number) {
+        this.current = number;
+    }
 }
