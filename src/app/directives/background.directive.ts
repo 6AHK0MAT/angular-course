b082e3cb9d2cb9ceda24a1dddcb83369d56f165a
6AHK0MAT <makhmutov.va@mail.ru>
2018-02-14 11:52:39 +0700

    Блок 4. Теория. Директивы Передача параметров

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,27 +1,30 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
-    selector: '[appBackrounnd]'
+    selector: '[appBackround]'
 })
 export class BackgroundDirective implements OnInit {
 
+    @Input('appBackround') hoverColor: string = 'green';
+    @Input() defaultColor: string = 'transparent';
     @HostBinding('style.backgroundColor') backround: string;
 
     constructor(private element: ElementRef, private renderer: Renderer2) {
     }
 
     ngOnInit() {
+        this.backround = this.defaultColor;
     }
 
     @HostListener('mouseenter') mouseEnter() {
         // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'red');
-        this.backround = 'green';
+        this.backround = this.hoverColor;
         // this.renderer.addClass(this.element.nativeElement, 'white-text');
     }
 
     @HostListener('mouseleave') mouseLeave() {
         // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'transparent');
-        this.backround = 'transparent';
+        this.backround = this.defaultColor;
         // this.renderer.removeClass(this.element.nativeElement, 'white-text');
     }
 }
