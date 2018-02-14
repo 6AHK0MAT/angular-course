6aefdcfa7e9a8dcbcc12f27c7bfa324f0a11e693
6AHK0MAT <makhmutov.va@mail.ru>
2018-02-14 10:45:05 +0700

    Блок 4. Теория. Директивы. Работа с событиями

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,15 +1,27 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appBackrounnd]'
 })
 export class BackgroundDirective implements OnInit {
+
+    @HostBinding('style.backgroundColor') backround: string;
+
     constructor(private element: ElementRef, private renderer: Renderer2) {
     }
 
     ngOnInit() {
-        this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
-        this.renderer.addClass(this.element.nativeElement, 'white-text');
-        // this.element.nativeElement.style.backgroundColor = 'red';
+    }
+
+    @HostListener('mouseenter') mouseEnter() {
+        // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'red');
+        this.backround = 'green';
+        // this.renderer.addClass(this.element.nativeElement, 'white-text');
+    }
+
+    @HostListener('mouseleave') mouseLeave() {
+        // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'transparent');
+        this.backround = 'transparent';
+        // this.renderer.removeClass(this.element.nativeElement, 'white-text');
     }
 }
