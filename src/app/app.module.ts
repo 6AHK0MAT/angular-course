2f5791e218095b5f47836fb22bc2c62e536443c3
6AHK0MAT <makhmutov.va@mail.ru>
2018-02-18 12:00:14 +0700

    Блок 5. Теория. Пайпы Создание динамического фильтра

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import {AppComponent} from './app.component';
-import {PowPipe} from './pow.pipe';
+import { AppComponent } from './app.component';
+import {PowPipe} from "./pow.pipe";
+import { CarFilterPipe } from './car-filter.pipe';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        PowPipe
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    PowPipe,
+    CarFilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
