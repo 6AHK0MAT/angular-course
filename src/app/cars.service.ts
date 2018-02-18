88b238faa9d76eafd01a07364a3b1a40e49f4851
6AHK0MAT <makhmutov.va@mail.ru>
2018-02-18 14:23:18 +0700

    Блок 6. Теория. Сервисы Использование сервиса в сервисе

--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -1,4 +1,11 @@
+import { ConsoleService } from './console.service';
+import { Injectable } from '@angular/core';
+
+@Injectable()
 export class CarsService {
+
+  constructor(private consoleService: ConsoleService) {}
+
   cars = [
     {
       name: 'Ford',
@@ -16,5 +23,6 @@ export class CarsService {
 
   addCar(name: string) {
     this.cars.push({isSold: false, name});
+    this.consoleService.log(`Машина ${name} была добавлена!`);
   }
 }
