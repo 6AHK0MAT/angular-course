b6512d1f0021305daa23a4e22992b3b0719dd133
6AHK0MAT <makhmutov.va@mail.ru>
2018-02-27 17:44:47 +0700

    Блок 7. Теория. Формы TD Значения по умолчанию

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    styles: [`
-    input.ng-invalid.ng-touched {
-      border: 1px solid red;
-    }
-  `]
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
     answers = [{
@@ -18,6 +13,9 @@ export class AppComponent {
         type: 'no',
         text: 'Нет'
     }];
+
+    defaultAnswer = 'no';
+    defaultCountry = 'ua';
 
     submitForm(form: NgForm) {
         console.log('Submited!', form);
