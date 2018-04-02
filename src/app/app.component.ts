efcdfe028a0f5b77fc06bb009be836ddb15fa008
6AHK0MAT <makhmutov.va@mail.ru>
2018-04-02 19:32:01 +0700

    Блок 11. Теория. Анимации Создание анимации

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('clickedDiv', [
+      state('start', style({
+        backgroundColor: 'blue',
+        width: '150px',
+        height: '150px',
+        marginLeft: '50px'
+      })),
+      state('end', style({
+        backgroundColor: 'red',
+        width: '300px',
+        height: '300px',
+        borderRadius: '50%',
+        marginLeft: '50px'
+      })),
+      transition('start => end', animate(1500)),
+      transition('end => start', animate('800ms 0.5s ease-out'))
+    ])
+  ]
 })
 export class AppComponent {
+  clickedDivState = 'start';
 
+  changeDivState() {
+    this.clickedDivState = 'end';
+    setTimeout(() => {
+      this.clickedDivState = 'start';
+    }, 3000);
+  }
 }
